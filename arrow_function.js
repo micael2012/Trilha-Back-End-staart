68a511fc60831d247183a763a8c1547a0470b2c6
Micael Itairã <tecmidiainfor@gmail.com>
2022-07-21 19:11:32 -0300

    add arquivos com exemplos  básicos de arrow functions

---
+++ arrow_function.js
@@ -0,0 +1,37 @@
+//JAVASCRIPT  ARROW FUNCTION
+// function temp(x = 0, y = 0) {
+//   return x + y;
+// }
+
+// let temp = (x, y) => {
+//   return x + y;
+// };
+
+// // console.log(temp(10,16));
+
+// let temp = (x = 0, y = 0) => x + y;
+
+// // console.log(temp(5+2));
+
+// let temp = () => {
+//   console.log('teste arrow functions');
+// };
+
+// // temp();
+
+// let temp = (texto) => console.log(texto);
+
+// temp('teste arrow funciton 2');
+
+// let temp = (texto) => console.log(texto);
+
+// temp('teste arrow functions 3');
+
+// const bandeiras = [
+//   ['Brasil', ['verde', 'Amarelo', 'Azul', 'Branco']],
+//   ['Canada', ['Verde', 'Branco']],
+//   ['Mexico', ['Verde', 'Branco', 'Vermelho']],
+//   ['EUA', ['Azul', 'Branco', 'Vermelho']],
+// ];
+
+// console.log(bandeiras[0][1][0]);
